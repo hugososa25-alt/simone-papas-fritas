66246a7d055bbfe654cf8ab3957208840addc676
hugososa25-alt <hugososa25@gmail.com>
2026-06-06 11:04:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const defaultData = {
     { id: 4, name: "Box de Papas", price: 9500, mode: "toppings", detail: "Papas fritas + toppings + salsas", image: "img/box_papas.png", active: true },
     { id: 5, name: "Box Premium Simone", price: 10000, mode: "solo_salsas", detail: "Papas fritas, patitas, formitas, bastones de mozzarella, caritas de papa y aros de cebolla", image: "img/hero_box.png", active: true },
     { id: 6, name: "Pollo Crujiente", price: 6000, mode: "solo_salsas", detail: "Pollo crujiente + salsas", image: "img/pollo_crujiente.png", active: true },
-    { id: 7, name: "Pollo Crujiente + Papas", price: 8000, mode: "solo_salsas", detail: "Pollo crujiente + papas fritas + salsas", image: "img/pollo_crujiente-papas.png", active: true }
+    { id: 7, name: "Pollo Crujiente + Papas", price: 8000, mode: "solo_salsas", detail: "Pollo crujiente + papas fritas + salsas", image: "img/pollo_crujiente_papas.png", active: true }
   ],
   toppings: [
     { id: 1, name: "Huevo picado en cubitos", image: "img/huevo_picado.png", active: true },
